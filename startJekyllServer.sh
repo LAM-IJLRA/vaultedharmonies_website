f5532cb00a9ea6d76519caa48dfb52eb447f5527
David Poirier-Q. <davipoir@gmail.com>
2025-01-11 21:23:05 +0100

    towards v1 of english website

---
+++ startJekyllServer.sh
@@ -0,0 +1,12 @@
+# for Bundler setup, see:
+# https://help.github.com/articles/setting-up-your-github-pages-site-locally-with-jekyll/
+#
+# basically (if ruby installed):
+# > gem install bundler
+# > bundle install
+
+# launch localhost http server
+bundle exec jekyll serve --baseurl '' --port 8000 --host=0.0.0.0 --livereload
+
+# open page in browser
+# open /Applications/Browsers/Google\ Chrome.app/ http://localhost:4000/
